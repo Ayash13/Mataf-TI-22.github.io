626b4e635db32312e8111d97a9f9a077b15cf991
Muhammad Ayash Al-Fatih <93539509+Ayash13@users.noreply.github.com>
2022-09-11 17:24:58 +0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -33,7 +33,7 @@ let header = $(`
    <!--<li class="nav-item nav-item-hover"><a class="nav-link" href="research.html">Research</a></li>-->
    <!--<li class="nav-item nav-item-hover"><a class="nav-link" href="education.html">Education</a></li>-->
    
-   <!--<li class="nav-item nav-item-hover"><a class="nav-link" href="Sponsor.html">Sponsor Page</a></li>-->
+   <!--<li class="nav-item nav-item-hover"><a class="nav-link" href="support.html">Our Sponsor</a></li>-->
    <li class="nav-item nav-item-hover"><a class="nav-link" href="https://myklass-eng.umy.ac.id" target="_blank">MyKlass</a></li>
    <li class="nav-item">
    <input type="checkbox" id="dark_toggler" class="dark_toggler" aria-label="Toggle Light Mode" onclick="toggle_light_mode()" checked>
